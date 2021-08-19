ad857f4ae14e02392d5c29a3ed43327f4c271282
Natsuki Wada <natsuki@MacBook-Air.lan>
2021-08-18 22:38:56 -0400

    added cookie and session functionality, fixed styling in frontend

--- client/containers/FeedContainer.jsx
+++ client/containers/FeedContainer.jsx
@@ -9,50 +9,38 @@ import {
   Typography,
   Toolbar,
   Link,
-  } from "@material-ui/core";
-  import { StylesProvider, styled } from '@material-ui/core/styles';
-  import Post from './../components/post.js';
+} from "@material-ui/core";
+import { StylesProvider, styled } from '@material-ui/core/styles';
+import Post from './../components/post.js';
 
+const AllPostsContainer = styled(Container)({
+  marginTop: '120px',
+})
 
-  const AllPostsContainer = styled(Container)({
-    //background: 'rgba(70, 70, 70, 0.7)',
-    marginTop: '120px',
-    
+class FeedContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
 
-  })
-
-  class FeedContainer extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {}
-    }
-
-    render (){
-
-      console.log(this.props.AppState.feed)
-
-      const feedArr = [];
-
-      this.props.AppState.feed.reverse().forEach((el,ind) => {
-        feedArr.push(<Post 
+  render() {
+    const feedArr = [];
+    this.props.AppState.feed.reverse().forEach((el, ind) => {
+      feedArr.push(
+        <Post 
           appState={this.props.AppState}
           key={ind}
           postProps={el} 
           update={this.props.update}
-          />)
-      })
+        />)
+    })
 
-
-      return(
-      // <div id='feedContainer'>
-      //   <Post />
-      // </div>
+    return (
       <AllPostsContainer id='feed' maxWidth='md'> 
-        {feedArr}
-        </AllPostsContainer >
-      // </div>
-      )
-    }
+       {feedArr}
+      </AllPostsContainer>
+    )
   }
+}
 
 export default FeedContainer;
